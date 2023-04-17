fca1550a4ac0c52204048bfe68c470a52728f1ed
Denys07748 <denis07748@gmail.com>
2023-04-17 12:43:26 +0300

    рефакторинг App и модалки на хуки

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import { useEffect, useState } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { Container } from './App.styled';
@@ -9,91 +9,100 @@ import Button from 'components/Button/Button';
 import Loader from 'components/Loader/Loader';
 import Modal from 'components/Modal/Modal';
 
-class App extends Component {
-  state = {
-    imagesData: [],
-    value: '',
-    page: 1,
-    isLoading: false,
-    totalImages: 0,
-    error: '',
-    showModal: false,
-    imageInfo: {
-      largeImg: '',
-      tag: '',
-    },
-  }
+const App = () => {
+  const [imagesData, setImagesData] = useState([]);
+  const [value, setValue] = useState(0);
+  const [page, setPage] = useState(1);
+  const [isLoading, setIsLoading] = useState(false);
+  const [totalImages, setTotalImages] = useState(0);
+  const [error, setError] = useState('');
+  const [showModal, setShowModal] = useState(false);
+  const [largeImg, setLargeImg] = useState('');
+  const [tagsImg, setTagsImg] = useState('');
 
-  componentDidUpdate(_, prevState) {
-    const { value, page } = this.state
+  // componentDidUpdate(_, prevState) {
+  //   const { value, page } = this.state
 
+  //   if(value === '') {
+  //     toast.warn('The input field cannot be empty.');
+  //     return;
+  //   }
+
+  //    if (prevState.value !== value || prevState.page !== page) {
+  //     this.getImages(value, page);
+  //    } 
+  // }
+
+  useEffect(() => {
     if(value === '') {
       toast.warn('The input field cannot be empty.');
       return;
     }
 
-     if (prevState.value !== value || prevState.page !== page) {
-      this.getImages(value, page);
-     } 
-  }
+    getImages(value, page);
+  }, [value, page]);
 
-  getImages = async (query, page) => {
-    this.setState({isLoading: true})
+
+
+  const getImages = async (query, page) => {
+    setIsLoading(true);
     try {
       const images = await API.getImages(query, page);
       if(images.hits.length === 0) {
-        this.setState({isLoading: false});
+        setIsLoading(false);
 
        return toast.error('Sorry, there are no images matching your search query. Please try again.');
       }
 
-        this.setState(state => 
-        ({ imagesData: [...state.imagesData, ...images.hits],
-           totalImages: images.totalHits }));
+        setImagesData(state => [...state, ...images.hits]);
+        setTotalImages(images.totalHits);
     } catch (error) {
-      this.setState({ error })
+      setError(error);
     } finally {
-      this.setState({isLoading: false});
+      setIsLoading(false);
     }   
   }
 
-  handleSearch = (value) => {
-    if(this.state.value === value) {
+  const handleSearch = (query) => {
+    if(value === query) {
       return;
     }
-    this.setState({ value, page: 1, imagesData: [] });   
+    setValue(query);
+    setPage(1);
+    setImagesData([]); 
   } 
 
-  onLoadMore = () => {
-    this.setState(state => ({page: state.page + 1}));
+  const onLoadMore = () => {
+    setPage(state => state + 1);
   }
 
-  onOpenModal = (largeImg, tag) => {
-      this.setState({ showModal: true, imageInfo: { largeImg, tag } });
+  const onOpenModal = (largeImg, tagsImg) => {
+    setShowModal(true);
+    setLargeImg(largeImg);
+    setTagsImg(tagsImg);
   }
 
-  onCloseModal = () => {
-    this.setState({ showModal: false, largeImg: '' });
+  const onCloseModal = () => {
+    setShowModal(false);
+    setLargeImg('');
+    setTagsImg('');
   }
 
-  render() {
-    const { imagesData, isLoading, error, totalImages, showModal, imageInfo } = this.state;
-    const totalPage = totalImages/imagesData.length;
+  const totalPage = totalImages/imagesData.length;
 
     return (
       <Container>
-        <Searchbar onSubmit={this.handleSearch}/>
+        <Searchbar onSubmit={handleSearch}/>
         {imagesData.length > 0 && 
-        (<ImageGallery images={imagesData} onOpenModal={this.onOpenModal}/>)}
+        (<ImageGallery images={imagesData} onOpenModal={onOpenModal}/>)}
         {totalPage > 1 && !isLoading && imagesData.length > 0 &&
-            <Button onLoadMore={this.onLoadMore}/>}
-        {showModal && <Modal onCloseModal={this.onCloseModal} imageInfo={imageInfo}/>}
+            <Button onLoadMore={onLoadMore}/>}
+        {showModal && <Modal onCloseModal={onCloseModal} largeImg={largeImg} tagsImg={tagsImg}/>}
         {isLoading && <Loader/>}
         {error && toast.error("Oops, an error occurred while loading the page. Please try reloading the page")}
         <ToastContainer autoClose={3000} theme="colored"/>
       </Container>
     );
-  }
 };
 
 export default App;
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,48 +1,44 @@
 import PropTypes from 'prop-types';
-import React, { Component } from 'react';
+import { useEffect } from 'react';
 import { createPortal } from 'react-dom';
 import { Overlay, ModalEl } from "./Modal.styled";
 
 const modalRoot = document.querySelector('#modal-root');
 
-class Modal extends Component {
-    componentDidMount() {
-        window.addEventListener('keydown', this.handleKeyDown);
-    }
+const Modal = ({onCloseModal, largeImg, tagsImg}) => {
+    useEffect(() => {
+        window.addEventListener('keydown', handleKeyDown);
 
-    componentWillUnmount() {
-        window.removeEventListener('keydown', this.handleKeyDown);
-    }
+        return () => {
+            window.removeEventListener('keydown', handleKeyDown);
+        };
+    },[]);
 
     handleKeyDown = e => {
             if(e.code === 'Escape') {
-                this.props.onCloseModal();
+                onCloseModal();
             }
     }
 
     handleBackdropClick = e => {
         if(e.currentTarget === e.target) {
-            this.props.onCloseModal();
+            onCloseModal();
         }
     }
 
-    render() {
-        const { largeImg, tags } = this.props.imageInfo;
-
-        return createPortal(
-            <Overlay onClick={this.handleBackdropClick}>
-                <ModalEl>
-                    <img src={largeImg} alt={tags} />
-                </ModalEl>
-            </Overlay>, modalRoot
-        );
-    }
+    return createPortal(
+        <Overlay onClick={handleBackdropClick}>
+            <ModalEl>
+                <img src={largeImg} alt={tagsImg} />
+            </ModalEl>
+        </Overlay>, modalRoot
+    );
 }
 
 Modal.propTypes = {
         onCloseModal: PropTypes.func.isRequired,
         largeImg: PropTypes.string.isRequired,
-        tags: PropTypes.string.isRequired,
+        tagsImg: PropTypes.string.isRequired,
     };
 
 export default Modal;
